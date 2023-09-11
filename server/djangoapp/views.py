5bda5ef3b96619b5c64cc01369127fcfd334a3ab
juanpavon <jp.von.landwust@gmail.com>
2023-09-11 19:43:53 -0400

    Corrected views.py and urls.py according to Pylint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+"""Django App Views"""
+import logging
+import json
+from datetime import datetime
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
@@ -6,9 +9,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 # from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from datetime import datetime
-import logging
-import json
+from django.core.exceptions import ObjectDoesNotExist
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -19,19 +20,19 @@ logger = logging.getLogger(__name__)
 
 # Create an `about` view to render a static about page
 def about(request):
+    """Returns the rendered 'About Us' page"""
     context = {}
-    if request.method == 'GET':
-        return render(request=request, template_name='djangoapp/about.html', context=context)
-
+    return render(request=request, template_name='djangoapp/about.html', context=context)
 
 # Create a `contact` view to return a static contact page
 def contact(request):
+    """Returns the rendered 'Contact Us' page"""
     context = {}
-    if request.method == 'GET':
-        return render(request=request, template_name='djangoapp/contact.html', context=context)
+    return render(request=request, template_name='djangoapp/contact.html', context=context)
 
 # Create a `login_request` view to handle sign in request
 def login_request(request):
+    """Logins the user and returns the rendered 'Login' page"""
     context = {}
     if request.method == 'POST':
         username = request.POST['username']
@@ -40,23 +41,21 @@ def login_request(request):
         if user is not None:
             login(request=request, user=user)
             return redirect('djangoapp:index')
-        else:
-            context['message'] = 'Invalid username or password'
-            return render(request=request, template_name='djangoapp/login.html', context=context)
-    else:
+        context['message'] = 'Invalid username or password'
         return render(request=request, template_name='djangoapp/login.html', context=context)
+    return render(request=request, template_name='djangoapp/login.html', context=context)
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
+    """Logouts the user and redirects to the index page"""
     logout(request=request)
     return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
+    """Registers a user and returns the rendered 'Registration' page"""
     context = {}
-    if request.method == 'GET':
-        return render(request=request, template_name='djangoapp/registration.html', context=context)
-    elif request.method == 'POST':
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         first_name = request.POST['firstname']
@@ -65,22 +64,23 @@ def registration_request(request):
         try:
             User.objects.get(username=username)
             user_exist = True
-        except:
-            logger.debug(f'{username} is new user')
+        except ObjectDoesNotExist:
+            logger.debug('%s is new user', username)
         if not user_exist:
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                            password=password)
+            user = User.objects.create_user(username=username, first_name=first_name,
+                                            last_name=last_name, password=password)
             login(request=request, user=user)
             return redirect('djangoapp:index')
-        else:
-            context['message'] = 'User already exists.'
-            return render(request=request, template_name='djangoapp/registration.html', context=context)
+        context['message'] = 'User already exists.'
+        return render(request=request, template_name='djangoapp/registration.html',
+                        context=context)
+    return render(request=request, template_name='djangoapp/registration.html', context=context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    """Returns the rendered 'Index' page"""
     context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+    return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
@@ -90,4 +90,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
